b139d6fe7049c3a45baf7ff4fd638fba78b929d2
Jacktivity <jh140695@gmail.com>
2017-02-07 20:29:25 +0000

    Quite a bit
    see for yourself, movement, turrets need work

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -9,18 +9,20 @@ public class TestScript : MonoBehaviour {
 	public bool onRight;
 	public bool onMid;
 	public int speed;
+	public float rotation;
 
 	// Use this for initialization
 	void Start () 
 	{
 		speed = 6;
+		rotation = 45;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		onLeft = (transform.position.x < -1.6);
-		onRight = (transform.position.x > 1.6);
+		onLeft = (transform.position.x < -16);
+		onRight = (transform.position.x > 16);
 		onMid = (transform.position.x > -0.1 && transform.position.x <0.1);
 	
 		if(Input.GetKeyDown(KeyCode.D))
@@ -31,11 +33,13 @@ public class TestScript : MonoBehaviour {
 				onLeft = false;
 				onMid = false;
 				GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, GetComponent<Rigidbody2D>().velocity.y);
+				transform.rotation = Quaternion.Euler(0,0,-rotation);	
 			}
 			else
 			{
 				turnLeft = false;
 			}
+	
 
 		}
 		if(Input.GetKeyDown(KeyCode.A))
@@ -46,23 +50,27 @@ public class TestScript : MonoBehaviour {
 				onRight = false;
 				onMid = false;
 				GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed, GetComponent<Rigidbody2D> ().velocity.y);
+				transform.rotation = Quaternion.Euler(0,0,rotation);	
 			} 
 			else 
 			{
 				turnRight = false;
 			}
+
 		}
 		if (onLeft) 
 		{
 			turnLeft = false;
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
-			transform.position = new Vector2(-1.6f, GetComponent<Rigidbody2D>().position.y);
+			transform.position = new Vector2(-16f, GetComponent<Rigidbody2D>().position.y);
+			transform.rotation = Quaternion.Euler(0,0,0);	
 		}
 		if (onRight) 
 		{
 			turnRight = false;
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
-			transform.position = new Vector2(1.6f, GetComponent<Rigidbody2D>().position.y);
+			transform.position = new Vector2(16f, GetComponent<Rigidbody2D>().position.y);
+			transform.rotation = Quaternion.Euler(0,0,0);
 		}
 		if (onMid) 
 		{
@@ -70,10 +78,12 @@ public class TestScript : MonoBehaviour {
 			turnLeft = false;
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
 			transform.position = new Vector2(0.0f, GetComponent<Rigidbody2D>().position.y);
+			transform.rotation = Quaternion.Euler(0,0,0);
 		}
 		if(!turnLeft && !turnRight)
 		{
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
 		}
+	
 	}
 }
